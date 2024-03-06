5748a3e7d5a2072f5497c6944fbf2f15645e2e58
NineTears <hellowind@live.com>
2024-03-06 15:12:15 +0800

    更新 TearsUI.lua

--- TearsUI.lua
+++ TearsUI.lua
@@ -15,39 +15,36 @@
         end
     end)
 
+    -- 设置frame层级
+    local function SetFrameStrataToLow(frame)
+        if frame then
+            frame:SetFrameStrata("Low")
+        end
+    end
 
-    --- 设置宠物动作条的层级
-    PetActionBarFrame:SetFrameStrata("LOW")
+    -- 设置宠物动作条的层级
+    SetFrameStrataToLow(PetActionBarFrame)
 
     -- 设置姿态条的层级
-    ShapeshiftBarFrame:SetFrameStrata("LOW")
+    SetFrameStrataToLow(ShapeshiftBarFrame)
 
     -- 设置主菜单栏艺术框架的层级
-    MainMenuBarArtFrame:SetFrameStrata("LOW")
+    SetFrameStrataToLow(MainMenuBarArtFrame)
 
     -- 设置施法条的层级
-    CastingBarFrame:SetFrameStrata("LOW")
+    SetFrameStrataToLow(CastingBarFrame)
 
     -- 设置主菜单经验条的层级
-    MainMenuExpBar:SetFrameStrata("LOW")
+    SetFrameStrataToLow(MainMenuExpBar)
 
     -- 设置主菜单栏的层级
-    MainMenuBar:SetFrameStrata("LOW")
+    SetFrameStrataToLow(MainMenuBar)
 
     -- 设置动作条按钮的层级
     for i = 1, NUM_ACTIONBAR_BUTTONS do
-      local button = getglobal("ActionButton" .. i)
-      button:SetFrameStrata("LOW")
-
-      button = getglobal("MultiBarBottomRightButton" .. i)
-      button:SetFrameStrata("LOW")
-
-      button = getglobal("MultiBarBottomLeftButton" .. i)
-      button:SetFrameStrata("LOW")
-
-      button = getglobal("MultiBarLeftButton" .. i)
-      button:SetFrameStrata("LOW")
-
-      button = getglobal("MultiBarRightButton" .. i)
-      button:SetFrameStrata("LOW")
+        SetFrameStrataToLow(_G["ActionButton" .. i])
+        SetFrameStrataToLow(_G["MultiBarBottomRightButton" .. i])
+        SetFrameStrataToLow(_G["MultiBarBottomLeftButton" .. i])
+        SetFrameStrataToLow(_G["MultiBarLeftButton" .. i])
+        SetFrameStrataToLow(_G["MultiBarRightButton" .. i])
     end
